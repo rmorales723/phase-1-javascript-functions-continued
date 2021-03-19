c9d5a356bcc0c4603570489df497d968bd0dda1e
robert morales <rmoralfit@gmail.com>
2021-03-18 20:10:41 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+  }
+
+  let mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+  }
+
+  let wrapAdjective = function(boss="*") {
+    return function(adjective="special") {
+      return `You are ${boss}${adjective}${boss}!`
+    }
+  }
